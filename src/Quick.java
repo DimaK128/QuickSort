f0d127bc803782909ecaefd7e6dcc4a041d88385
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2024-05-18 14:18:35 +0300

    !

--- src/Quick.java
+++ src/Quick.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class F {
+public class Quick {
+    public static void sort(Comparable[] a) {
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+
+    private static int partition(Comparable[] a, int lo, int hi) {
+        Comparable v = a[lo];
+        int i = lo, j = hi + 1;
+        while (true) {
+            while (less(a[++i], v))
+                if (i == hi) break;
+            while (less(v, a[--j]))
+                if (j == lo) break;
+            if (i >= j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] testArray = {3, 6, 8, 1, 5, 2, 9, 7, 4};
+        System.out.println("Original Array:");
+        printArray(testArray);
+        sort(testArray);
+        System.out.println("Sorted Array:");
+        printArray(testArray);
+    }
+
+    private static void printArray(Comparable[] a) {
+        for (Comparable comparable : a) {
+            System.out.print(comparable + " ");
+        }
+        System.out.println();
+    }
 }
